63130047086e7be5fd06362900a1b4e82d57439c
Vithusan Kanageswaran <vk@Vithusans-MacBook-Air.local>
2019-12-10 10:37:06 -0500

    created new components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom'
 import './App.css';
+import Homepage from './component/Homepage'
 
 function App() {
   return (
     <Router>
       <Switch>
-        <Route exact path='/' component={} />
+        <Route exact path='/' component={Homepage} />
       </Switch>
     </Router>
   );
---
+++ src/component/Homepage.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class Homepage extends Component {
+    render() {
+        return (
+            <div>
+                <div className='centerDiv'>
+                    <h1 className="animated fadeInUp">VITHUSAN</h1>
+                    <h2 className="animated fadeInUp delay-1s">Software Engineer</h2>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default Homepage;
