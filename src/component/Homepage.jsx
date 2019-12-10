dfc66e453d231ee6b4234a10945d87466d50961b
Vithusan Kanageswaran <vk@Vithusans-MacBook-Air.local>
2019-12-10 11:24:21 -0500

    installed material design icon

--- src/component/Homepage.jsx
+++ src/component/Homepage.jsx
@@ -3,10 +3,13 @@ import React, { Component } from 'react';
 class Homepage extends Component {
     render() {
         return (
-            <div>
+            <div className='mainDiv'>
                 <div className='centerDiv'>
-                    <h1 className="animated fadeInUp">VITHUSAN</h1>
-                    <h2 className="animated fadeInUp delay-1s">Software Engineer</h2>
+                    <h1 className="animated fadeInDown">VITHUSAN</h1>
+                    <h2 className="animated fadeInDown delay-0.5s">Software Engineer</h2>
+                    <p className="animated fadeInDown delay-0.5s aboutTag">Detail-oriented software engineer with proficient in JavaScript,
+                        React and Node.Js.  Experienced building web apps in fast-paced
+                        and collaborative working environments. </p>
                 </div>
             </div>
         );
