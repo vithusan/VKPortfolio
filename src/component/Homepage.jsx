2b274f963c186683289bf482791f8bf5b46e2522
Vithusan Kanageswaran <vk@Vithusans-MacBook-Air.local>
2019-12-10 13:28:53 -0500

    aligned everything and imported arrow icons

--- src/component/Homepage.jsx
+++ src/component/Homepage.jsx
@@ -5,11 +5,18 @@ class Homepage extends Component {
         return (
             <div className='mainDiv'>
                 <div className='centerDiv'>
-                    <h1 className="animated fadeInDown">VITHUSAN</h1>
-                    <h2 className="animated fadeInDown delay-0.5s">Software Engineer</h2>
-                    <p className="animated fadeInDown delay-0.5s aboutTag">Detail-oriented software engineer with proficient in JavaScript,
-                        React and Node.Js.  Experienced building web apps in fast-paced
+                    <i className="material-icons md-70 animated fadeInLeft delay-2s">keyboard_arrow_left</i>
+                    <div className='aboutDiv'>
+                        <h1 className="animated fadeInDown delay-1s">VITHUSAN</h1>
+                        <h2 className="animated fadeInDown delay-2s">Software Engineer</h2>
+                        <p className="animated fadeInDown delay-2s aboutTag">Detail-oriented software engineer with proficient in JavaScript,
+                            React and Node.Js.  Experienced building web apps in fast-paced
                         and collaborative working environments. </p>
+                    </div>
+                    <i className="material-icons md-70 animated fadeInRight delay-2s">keyboard_arrow_right</i>
+                </div>
+                <div className='arrow_down'>
+                    <i className="material-icons md-70 animated fadeInUp delay-2s">keyboard_arrow_down</i>
                 </div>
             </div>
         );
