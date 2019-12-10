6438c3bfb4db9919d8f4e4f10b536bdac0f96085
Vithusan Kanageswaran <vk@Vithusans-MacBook-Air.local>
2019-12-10 10:43:30 -0500

    added route to other components

--- src/App.js
+++ src/App.js
@@ -2,12 +2,19 @@ import React from 'react';
 import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom'
 import './App.css';
 import Homepage from './component/Homepage'
+import Portfolio from './component/Portfolio'
+import Skills from './component/Skills'
+import Contact from './component/Contact'
+
 
 function App() {
   return (
     <Router>
       <Switch>
         <Route exact path='/' component={Homepage} />
+        <Route exact path='/portfolio' component={Portfolio} />
+        <Route exact path='/skill' component={Skills} />
+        <Route exact path='/contact' component={Contact} />
       </Switch>
     </Router>
   );
