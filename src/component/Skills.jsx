4e7d8436652515cef006a7286ae604d16ba15505
Vithusan Kanageswaran <vk-macmini@Vithusans-Mac-mini.local>
2021-07-21 13:57:25 -0700

    Added some skills and edited the skills page

--- src/component/Skills.jsx
+++ src/component/Skills.jsx
@@ -14,20 +14,25 @@ class Skills extends Component {
                 </div>
                 <div className='skillCenterDiv animated fadeInUp'>
                     <div className='listOfSkills'>
-                        <i className="devicon-react-original-wordmark languageIcon"></i>
-                        <i className="devicon-python-plain-wordmark languageIcon"></i>
-                        <i className="devicon-postgresql-plain-wordmark languageIcon"></i>
-                        <i className="devicon-nodejs-plain-wordmark languageIcon"></i>
-                        <i className="devicon-jquery-plain-wordmark languageIcon"></i>
-                        <i className="devicon-javascript-plain languageIcon"></i>
-                        <i className="devicon-html5-plain-wordmark languageIcon"></i>
-                        <i className="devicon-heroku-original-wordmark languageIcon"></i>
-                        <i className="devicon-github-plain-wordmark languageIcon"></i>
-                        <i className="devicon-git-plain-wordmark languageIcon"></i>
-                        <i className="devicon-express-original languageIcon"></i>
-                        <i className="devicon-django-plain languageIcon"></i>
-                        <i className="devicon-css3-plain-wordmark languageIcon"></i>
-                        <i className="devicon-amazonwebservices-plain-wordmark languageIcon"></i>
+                        <i className="devicon-react-original languageIcon"><p className="iconDes" >React</p></i>
+                        <i className="devicon-redux-original languageIcon"><p className="iconDes" >Redux</p></i>
+                        <i className="devicon-docker-plain languageIcon"><p className="iconDes" >Docker</p></i>
+                        <i className="devicon-mongodb-plain languageIcon"><p className="iconDes" >MongoDB</p></i>
+                        <i className="devicon-express-original languageIcon"><p className="iconDes" >Express.js</p></i>
+                        <i className="devicon-nodejs-plain languageIcon"><p className="iconDes" >Node.js</p></i>
+                        <i className="devicon-javascript-plain languageIcon"><p className="iconDes" >JavaScript</p></i>
+                        <i className="devicon-python-plain languageIcon"><p className="iconDes" >Python</p></i>
+                        <i className="devicon-django-plain languageIcon"><p className="iconDes" >Django</p></i>
+                        <i className="devicon-postgresql-plain languageIcon"><p className="iconDes" >PostgreSQL</p></i>
+                        <i className="devicon-html5-plain languageIcon"><p className="iconDes" >HTML5</p></i>
+                        <i className="devicon-css3-plain languageIcon"><p className="iconDes" >CSS3</p></i>
+                        <i className="devicon-heroku-original languageIcon"><p className="iconDes" >Heroku</p></i>
+                        <i className="devicon-github-plain languageIcon"><p className="iconDes" >GitHub</p></i>
+                        <i className="devicon-git-plain languageIcon"><p className="iconDes" >Git</p></i>
+                        <i className="devicon-amazonwebservices-plain languageIcon"><p className="iconDes" >AWS</p></i>
+                        <i className="devicon-linux-plain languageIcon"><p className="iconDes" >Linux</p></i>
+                        
+                        
                     </div>
                 </div>
             </div>
